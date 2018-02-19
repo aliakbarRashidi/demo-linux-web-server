c128024a1d7d7c7c76ef2fa1f293a2d12eff8008
Alexei Lozovsky <lozovsky@apriorit.com>
2018-02-19 14:26:41 +0200

    Initial project
    Throw in a basic hello world to get started.

---
+++ README.rst
@@ -0,0 +1,12 @@
+######################
+Demo: Linux Web Server
+######################
+
+A simple web server demonstrating Linux APIs for networking and threading.
+
+License
+=======
+
+The code is licensed under `MIT license`_.
+
+.. _`MIT license`: LICENSE
---
+++ src/demo_web_server.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018, Apriorit, Inc.
+ *
+ * Demo: Linux Web Server
+ *
+ * Licensed under MIT license (see LICENSE in the root directory). This file
+ * may be copied, distributed, and modified only in accordance with the terms
+ * specified by the license.
+ */
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, world!\n";
+}
